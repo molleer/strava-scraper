6ad744c762f2c904cc646df11a4096f0890ada52
David Möller <damoller99@hotmail.se>
2020-11-30 16:12:24 +0100

    Can pase site but does not like html, expects pure xml

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const parseString = require("xml2js").parseString;
 
 const app = express();
+app.use(bodyParser.text({ limit: "100mb" }));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(
+    '<html>  \
+  <head></head> \
+  <body> \
+  <input type="text"> \
+  <ol> \
+  <li>Hi there</li> \
+  <li>Hello there</li> \
+  </ol></body>\
+  </html>',
+  );
 });
+
+app.post("/update", (req, res) => {
+  parseString("<body>" + req.body + "</body>", (err, body) => {
+    if (err) console.log(err);
+    console.log(body);
+  });
+  //console.log(req.body);
+  res.status(200);
+});
+
+/**
+var req = new XMLHttpRequest();
+req.open("POST", "http://localhost:8080/update");
+req.setRequestHeader("Content-Type", "text/plain");
+req.send(document.body.innerHTML);
+ */
 
 app.listen(8080);
